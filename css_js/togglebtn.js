ce21c922377a473639a562b03ee10451a25be745
Karen S <58454787+cat-theConqveror@users.noreply.github.com>
2020-06-07 12:49:15 +0100

    Update side navigation

--- css_js/togglebtn.js
+++ css_js/togglebtn.js
@@ -17,3 +17,21 @@ window.onclick = function(event) {
 		}
 	}
 }
+
+function myMobFunction() {
+	document.getElementById("myDropdownMob").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
